4d93e6a83b70105136c920bc62ab25300f09abf2
z6842001 <40972186+z6842001@users.noreply.github.com>
2018-07-09 11:18:24 +0800

    Game_Other

---
+++ Game_Other/Other.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Game_Other
+{
+     class Other
+    {
+        Bitmap[] map;
+
+
+        public Other(Bitmap[] y,Size size)
+        {
+
+            
+            int X = y.Length;
+            map = new Bitmap[X](80, 80);
+
+
+
+            for(int i = )
+            
+
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+}
